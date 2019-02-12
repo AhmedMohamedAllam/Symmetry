ade6059333df78598b69b2628e0ffaacd90b1e49
Ahmed Allam <ahmed.allam@thed.io>
2019-02-12 20:47:58 +0200

    Add assets

---
+++ Symmetry/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  Symmetry
+//
+//  Created by Allam on 11/3/18.
+//  Copyright © 2018 Allam. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
