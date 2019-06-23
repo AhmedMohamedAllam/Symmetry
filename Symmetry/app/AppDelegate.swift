afe524035b3aa784004d7b3e781c057fd0197944
Ahmed Allam <ahmed.allam@thed.io>
2019-06-23 21:54:55 +0200

    activate all orientation modes but return in app delegate .portrait only

--- Symmetry/app/AppDelegate.swift
+++ Symmetry/app/AppDelegate.swift
@@ -14,4 +14,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         return true
     }
+    
+    func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
+        return .portrait
+    }
 }
